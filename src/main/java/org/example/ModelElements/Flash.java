a9eef7cc24a6e34619628d94ccc63814186d9ecd
AnnaKul <89269690212@mail.ru>
2023-07-30 12:31:37 +0300

    HOMEWORK 1 ARCHITECTOR

---
+++ src/main/java/org/example/ModelElements/Flash.java
@@ -0,0 +1,33 @@
+package org.example.ModelElements;
+
+import org.example.Staff.Angle3D;
+import org.example.Staff.Point3D;
+import java.awt.*;
+
+/**
+ * класс источников света
+ */
+
+public class Flash {
+    public Point3D location;
+    public Angle3D angle;
+    public Color color;
+    public float power;
+
+    /**
+     * поворот источников света на угол
+     *
+     * @param angleAction
+     */
+    public void rotate(Angle3D angleAction){
+
+    }
+    /**
+     * перемещение к точке
+     * @param pointAction
+     */
+
+    public void move (Point3D pointAction){
+
+    }
+}
---
+++ src/main/java/org/example/ModelElements/Scene.java
@@ -0,0 +1,45 @@
+package org.example.ModelElements;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scene {
+    public int id;
+    public List<PoligonalModel> models;
+    public List<Flash> flashes;
+    public List<Camera> cameras = new ArrayList<Camera>();
+
+    public Scene(int id, List<PoligonalModel> models, List<Flash> flashes, List<Camera> cameras) throws Exception {
+
+        this.id = id;
+        if (models.size() > 0){
+            this.models = models;
+        } else {
+            throw new Exception ("Должна быть одна модель");
+        }
+        this.flashes = flashes;
+        if (cameras.size() > 0){
+            this.cameras = cameras;
+        } else {
+            throw new Exception ("Должна быть одна модель");
+        }
+    }
+    public <T> T method1(T flash){
+        return flash;
+    }
+    public <T, E> T method2(T model, E Flash){
+        return model;
+    }
+    // /**
+    // * заглушка
+    // */
+    // public Type1 method1(Type1 t) {
+    // return t;
+    // }
+
+    // /**
+    // * заглушка
+    // */
+    // public Type1 method1(Type1 t1, Type2 t2) {
+    // return t1;
+    // }
+}
